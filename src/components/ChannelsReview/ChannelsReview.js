80327c3751d1619758ee6e420759008ff14031da
Tony <tonyivanovak@gmail.com>
2023-11-22 00:24:22 +0200

    Fixed quorterly distribution

--- src/components/ChannelsReview/ChannelsReview.js
+++ src/components/ChannelsReview/ChannelsReview.js
@@ -14,16 +14,28 @@ const ChannelsReview = () => {
   );
 
   const getChannelValues = (channel) => {
-    return budgetDistributions
-      .find((distribution) => distribution.budgetId === channel.budgetId)
-      .distribution.map((value, index) => {
-        return <td>${value.value}</td>;
+    let distribution = budgetDistributions.find(
+      (distribution) => distribution.budgetId === channel.budgetId
+    ).distribution;
+
+    // Calculate values for Quaterly distribution per month
+    if (distribution.length === 4) {
+      const newDistribution = [];
+      distribution.forEach((value) => {
+        const perMonth = (parseFloat(value.value) / 3).toFixed(0);
+        newDistribution.unshift(perMonth, perMonth, perMonth);
       });
+      distribution = newDistribution;
+    }
+
+    return distribution.map((value, index) => {
+      return <td key={index}>${value.value ? value.value : value}</td>;
+    });
   };
 
-  const tableValues = channels.map((channel) => {
+  const tableValues = channels.map((channel, index) => {
     return (
-      <tr>
+      <tr key={index}>
         <td
           className="channel-title"
           style={{
@@ -52,17 +64,20 @@ const ChannelsReview = () => {
   return (
     <table className="channels-table">
       <thead>
-        <th
-          style={{
-            backgroundImage: `url(${background})`,
-            backgroundRepeat: "repeat-y",
-            backgroundPositionX: "right",
-            borderRight: "2px solid #b2bbd550",
-          }}
-        >
-          Channel
-        </th>
-        {monthsHeaders}
+        <tr>
+          <th
+            style={{
+              backgroundImage: `url(${background})`,
+              backgroundRepeat: "repeat-y",
+              backgroundPositionX: "right",
+              borderRight: "2px solid #b2bbd550",
+            }}
+          >
+            Channel
+          </th>
+
+          {monthsHeaders}
+        </tr>
       </thead>
       <tbody>{tableValues}</tbody>
     </table>
